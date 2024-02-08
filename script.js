fe8588762f9666364efa6b965ebb2c35d6d57f82
Caetano Labaki <caetanolz@hotmail.com>
2024-02-08 13:06:00 -0300

    Tarefa1 - concluida
    Foi criado  o código que verifica se a palavra/frase é um palindrome, utilizando loop de repetição, condicioanais e retirando espaços.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function isPalindrome(str){
-    /* Seu código aqui */
-}
+    str = str.toUpperCase().replaceAll(" ", "");
+    let tamanhoPalavra = str.length;
+       for(let i = 0; i <= tamanhoPalavra; i++) {
+        if(str[i] != str[tamanhoPalavra - 1 - i]) {
+            return false;
+        }  
+        } return true;}
+
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
